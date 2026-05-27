e583d502cb140502c63bf5ce102f10c1285d54f3
root <root@Dede-Latitude-7420.localdomain>
2026-05-27 13:45:18 -0400

    changed text mentions of WXYC to WXDU in Blog page  and Breadcrumb

--- components/SingleCrumb.js
+++ components/SingleCrumb.js
@@ -4,10 +4,13 @@ import Link from 'next/link'
 // Each individual "crumb" in the breadcrumbs list
 export default function Crumb({text, href, last = false}) {
 	// Blog header on blog home page
-	if (last && text === 'WXDU PRESS') {
+	if (last && text === 'WXDU PRESS') { 
+		// I changed 'WXYC PRESS' to 'WXDU PRESS' in BlogBreadcrumbs.js, so this condition will never be true. Should I change it to 'WXDU PRESS' here?
+		// Does that break something?
 		return null
 	}
 	// Blog header when you're not on blog home page (links back to blog home page)
+	if (!last && text === 'WXDU PRESS') {
 	if (!last && text === 'WXDU PRESS') {
 		return (
 			<Link href={href}>
