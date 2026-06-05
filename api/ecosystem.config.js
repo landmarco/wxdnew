225d898a0fa55beb5d4a9130edea9512c30a3e11
Marc Loeffke <marc.loeffke@gmail.com>
2026-06-04 21:30:19 -0400

    updates ecosystem filepath & package-lock

--- api/ecosystem.config.js
+++ api/ecosystem.config.js
@@ -4,7 +4,7 @@ module.exports = {
       name: 'wxdu-api',
       script: 'server.js',
       // Update this path to wherever the repo lives on the server (under the computing user)
-      cwd: '/mnt/md1/wxdnew/api',
+      cwd: '/var/www/wxdnew/api',
       // Point at the nvm-installed node that is >= 14. After running
       //   nvm install 20 && nvm alias default 20
       // as the computing user, the path will be:
