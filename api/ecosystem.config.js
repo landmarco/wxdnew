92f83b12671f8a7f7b704cd7c1e0b8c0b2c0c155
landmarco <marc.loeffke@gmail.com>
2026-06-04 22:03:28 -0400

    updates ecosystem.config to work with node 16

--- api/ecosystem.config.js
+++ api/ecosystem.config.js
@@ -3,12 +3,8 @@ module.exports = {
     {
       name: 'wxdu-api',
       script: 'server.js',
-      // Update this path to wherever the repo lives on the server (under the computing user)
       cwd: '/var/www/wxdnew/api',
-      // Point at the nvm-installed node that is >= 14. After running
-      //   nvm install 20 && nvm alias default 20
-      // as the computing user, the path will be:
-      interpreter: '/home/computing/.nvm/versions/node/v20.0.0/bin/node',
+      interpreter: '/home/computing/.nvm/versions/node/v16.20.2/bin/node',
       watch: false,
       env: {
         NODE_ENV: 'production',
